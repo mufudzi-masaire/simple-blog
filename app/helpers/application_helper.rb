fb20fddfece9080629f845f1627a98dbcbd9eee0
MMas <mufudzimasaire@gmail.com>
2013-06-10 10:08:04 +0100

    Add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    site_name = "Blog"
+    if page_title.empty?
+      site_name 
+    else
+      "#{site_name} | #{page_title}"
+    end 
+  end
+  
 end
